57eba826f23120c2854b1a9bd7c850e9f2bedecb
Mayank Mittal <mayankmittal3009@gmail.com>
2026-04-03 21:27:36 +0530

    fixed server

--- server/server.js
+++ server/server.js
@@ -23,7 +23,9 @@ app.use(express.json());
 // Nodemailer Setup
 // -----------------------
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // use TLS
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
